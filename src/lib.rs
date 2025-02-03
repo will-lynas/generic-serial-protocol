c4c113a02c6c6c8013b6ca4a0defbf2722a1348e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-02-03 19:44:29 +0000

    Make the partial message test pass

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,18 @@ const START_BYTE: u8 = 0x58;
 const ESCAPE_BYTE: u8 = 0x42;
 const XOR_BYTE: u8 = 0x69;
 
+#[derive(Debug)]
+enum ReadError {
+    NewMessage,
+    Io(io::Error),
+}
+
+impl From<io::Error> for ReadError {
+    fn from(error: io::Error) -> Self {
+        ReadError::Io(error)
+    }
+}
+
 pub struct SerialManager<T>
 where
     T: Read + Write,
@@ -38,16 +50,24 @@ where
         Ok(())
     }
 
-    fn read_escaped_byte(&mut self) -> io::Result<u8> {
+    fn read_byte(&mut self) -> Result<u8, ReadError> {
         let mut byte = [0u8; 1];
         self.connection.read_exact(&mut byte)?;
 
-        if byte[0] == ESCAPE_BYTE {
-            let mut next_byte = [0u8; 1];
-            self.connection.read_exact(&mut next_byte)?;
-            Ok(next_byte[0] ^ XOR_BYTE)
+        if byte[0] == START_BYTE {
+            return Err(ReadError::NewMessage);
+        }
+        Ok(byte[0])
+    }
+
+    fn read_escaped_byte(&mut self) -> Result<u8, ReadError> {
+        let byte = self.read_byte()?;
+
+        if byte == ESCAPE_BYTE {
+            let next_byte = self.read_byte()?;
+            Ok(next_byte ^ XOR_BYTE)
         } else {
-            Ok(byte[0])
+            Ok(byte)
         }
     }
 
@@ -58,7 +78,7 @@ where
         Ok(())
     }
 
-    fn read_escaped_bytes(&mut self, length: usize) -> io::Result<Vec<u8>> {
+    fn read_escaped_bytes(&mut self, length: usize) -> Result<Vec<u8>, ReadError> {
         let mut result = Vec::with_capacity(length);
         for _ in 0..length {
             result.push(self.read_escaped_byte()?);
@@ -81,6 +101,7 @@ where
     }
 
     pub fn receive(&mut self) -> io::Result<Message> {
+        // Wait for the initial start byte
         loop {
             let mut byte = [0u8; 1];
             self.connection.read_exact(&mut byte)?;
@@ -89,6 +110,17 @@ where
             }
         }
 
+        // Try reading messages until we succeed
+        loop {
+            match self.read_message() {
+                Ok(message) => return Ok(message),
+                Err(ReadError::NewMessage) => (), // Got another START_BYTE, try reading new message
+                Err(ReadError::Io(e)) => return Err(e),
+            }
+        }
+    }
+
+    fn read_message(&mut self) -> Result<Message, ReadError> {
         // Read and unescape length
         let length_bytes = self.read_escaped_bytes(2)?;
         let length = u16::from_le_bytes([length_bytes[0], length_bytes[1]]) as usize;
